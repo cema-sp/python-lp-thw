e8b3f0bb0ddf8fcbd90fd6e14a2c9a7fceec448e
cema-sp <cema.rus@gmail.com>
2015-03-01 16:20:51 +0300

    Adding some code

--- ex037.py
+++ ex037.py
@@ -1,3 +1,5 @@
+from os import name
+
 pi_ = 3.1415
 
 class MyClass:
@@ -12,13 +14,18 @@ class MyClass:
   enums_dict_ = {"four": 4, "five": 5, "six": 6}
 
   def greet(self):
-    print "Greeting: %s" % self.hello_
+    return "Greeting: %s" % self.hello_
+  
+  def greet_me(self):
+    print "Enter your name: "
+    name_raw = raw_input("> ")
+    print "Greeting: %s, %s" % (self.hello_, name_raw)
 
   def unimplemented(self): pass
 
   def logic(self):
-    if self.f_:
-      print "%r is true." % self.f_
+    if not self.f_ and not self.n_:
+      print "%r is false, %r is false." % (self.f_, self.n_)
     elif self.n_:
       print "%r is true." % self.n_
     elif self.t_:
@@ -27,21 +34,74 @@ class MyClass:
       raise ValueError("ERROR")
 
   def radius(self):
-		print "Enter R: "
-		r_raw = raw_input("> ")
+    print "Enter R: "
+    r_raw = raw_input("> ")
+    try:
+      sqr = lambda x: x**2
+      area = pi_ * sqr(int(r_raw))
+      print "Area is %g." % area
+    except ValueError, e:
+      print "%r is not a valid integer (%s)!" % (r_raw, e)
+    finally:
+      print "Finished correctly"
 
-		try:
-			r = int(r_raw)
-			area = pi_ * r**2
-			print "Area is %g." % area
-		except ValueError, e:
-			print "%r is not a valid integer (%s)!" % (r_raw, e)
+  def loop(self, n):
+    assert n < 10, "FUUUUUU~~~~"
+		
+    if n < 0:
+      raise ValueError("n should be positive")
+    else: pass
+
+    for i in range(n):
+      if i is not 2:
+        print "i = %d" % i
+      else:
+        i += 1
+        continue
+      exec 'print "Next iteration!"'
+      if i is 7: break
+      else: pass
+
+  def call_loop(self):
+    print "Loop n: "
+    n_raw = raw_input("> ")
+    n = int(n_raw)
+    self.loop(n)
+
+class Controlled:
+  v_ = "value"
+
+  def __enter__(self):
+    print "calling __enter__"
+    self.v_ += " 1"
+  
+  def __exit__(self, type, value, traceback):
+    print "calling __exit__"
+
+def yield_function():
+  yield "HELLO"
+
+def yield_generator():
+	for i in range(3):
+		yield i**i
 
 # ----------- main ---------------
 
 my_object = MyClass()
-my_object.greet()
+print my_object.greet()
+my_object.greet_me()
 my_object.unimplemented()
 my_object.logic()
 my_object.radius()
+my_object.call_loop()
+
+with Controlled() as contr:
+  print "Value of \"v_\": %r" % "UNKNOWN"
+
+print "OS name: %r" % name
+
+yield_function().next()
+
+for i in yield_generator():
+	print "Generating: %d" % i
 
